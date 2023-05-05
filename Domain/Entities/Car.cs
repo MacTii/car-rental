22d941113cfa31025769ca63433ab06c3fb8e639
MacTii <mactii41@gmail.com>
2023-05-05 15:56:29 +0200

    feature: add car repository and modify car model

--- Domain/Entities/Car.cs
+++ Domain/Entities/Car.cs
@@ -9,8 +9,8 @@ namespace Domain.Entities
     public class Car
     {
         public int Id { get; set; }
-        public string? Make { get; set; }
-        public string? Model { get; set; }
-        public string? RegistrationNumber { get; set; }
+        public string Make { get; set; } = null!;
+        public string Model { get; set; } = null!;
+        public string RegistrationNumber { get; set; } = null!;
     }
 }
