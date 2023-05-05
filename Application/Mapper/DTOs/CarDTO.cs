f9509f006bcbc08890ba3134e6f0aa81c861b1ef
MacTii <mactii41@gmail.com>
2023-05-06 00:56:27 +0200

    feature: add dto models

---
+++ Application/Mapper/DTOs/CarDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mapper.DTOs
+{
+    public class CarDTO
+    {
+        public string Make { get; set; } = null!;
+        public string Model { get; set; } = null!;
+        public string RegistrationNumber { get; set; } = null!;
+    }
+}
---
+++ Application/Mapper/DTOs/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mapper.DTOs
+{
+    public class UserDTO
+    {
+        public string Name { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string? Email { get; set; }
+        public string PhoneNumber { get; set; } = null!;
+    }
+}
