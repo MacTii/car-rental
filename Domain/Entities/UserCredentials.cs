9fe031aa92325a254e870c553ac3a5a50ee3be13
MacTii <mactii41@gmail.com>
2023-05-08 19:03:37 +0200

    feature: user credentials implementation

---
+++ Domain/Entities/UserCredentials.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class UserCredentials
+    {
+        public int ID { get; set; }
+        public string Username { get; set; } = null!;
+        public byte[] PasswordHash { get; set; } = null!;
+        public byte[] PasswordSalt { get; set; } = null!;
+        public string? RefreshToken { get; set; }
+        public DateTime? TokenCreated { get; set; }
+        public DateTime? TokenExpires { get; set; }
+        public bool IsActive { get; set; } = true;
+        public UserRoleEnum UserRole { get; set; }
+    }
+}
