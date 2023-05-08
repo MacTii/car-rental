0b9a925f9e3e3fd36974e65a33eec6dc68695f6b
MacTii <mactii41@gmail.com>
2023-05-08 19:05:04 +0200

    feature: authentication implementation

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,64 @@
+﻿using Application.Interfaces;
+using Application.Mapper.DTOs;
+using Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IAuthService _authService;
+        private readonly IUserCredentialsService _userCredentialsService;
+
+        public AuthController(IConfiguration configuration, IAuthService authService, IUserCredentialsService userCredentialsService)
+        {
+            _configuration = configuration;
+            _authService = authService;
+            _userCredentialsService = userCredentialsService;
+        }
+
+        [HttpGet, Authorize]
+        public ActionResult GetMe()
+        {
+            var username = _userCredentialsService.GetMyName();
+            return Ok(username);
+        }
+
+        [HttpPost("register")]
+        public ActionResult Register([FromBody] RegisterDTO request)
+        {
+            if (request == null)
+                return BadRequest("Invalid input data"); // return 400 Bad Request
+
+            var user = _authService.Register(request);
+
+            return Ok(
+                new
+                {
+                    Response = "User registered successfully",
+                    Data = user
+                });
+        }
+
+        [HttpPost("login")]
+        public ActionResult Login(LoginDTO request)
+        {
+            if (request == null)
+                return BadRequest("Invalid input data"); // return 400 Bad Request
+
+            var token = _authService.Login(request);
+
+            return Ok(
+                new
+                {
+                    Response = "User logged in successfully",
+                    Data = token
+                });
+        }
+    }
+}
---
+++ Application/Services/AuthService.cs
@@ -0,0 +1,168 @@
+﻿using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Domain.Entities;
+using Application.Mapper.DTOs;
+using System.Security.Cryptography;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using AutoMapper;
+
+namespace Application.Services
+{
+    public class AuthService : IAuthService
+    {
+        #region Injection
+
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
+        private readonly IUserCredentialsRepository _userCredentialsRepository;
+        private readonly IUserRepository _userRepository;
+
+        public AuthService(IConfiguration configuration, IMapper mapper, IUserCredentialsRepository userCredentialsRepository,
+            IUserRepository userRepository)
+        {
+            _configuration = configuration;
+            _mapper = mapper;
+            _userCredentialsRepository = userCredentialsRepository;
+            _userRepository = userRepository;
+        }
+
+        #endregion Injection
+
+        public UserCredentials Register(RegisterDTO request)
+        {
+            var existingUser = _userCredentialsRepository.GetByUsername(request.Username);
+            if (existingUser != null)
+                throw new InvalidOperationException("This username is taken!");
+
+            if (request.FirstPassword != request.SecondPassword)
+                throw new ArgumentException("The passwords provided are not identical!");
+
+            CreatePasswordHash(request.FirstPassword, out byte[] passwordHash, out byte[] passwordSalt);
+
+            // mapping RegisterDTO -> UserCredentials
+            var userCredentials = _mapper.Map<UserCredentials>(request);
+            userCredentials.PasswordHash = passwordHash;
+            userCredentials.PasswordSalt = passwordSalt;
+
+            // mapping RegisterDTO -> User
+            var user = _mapper.Map<User>(request);
+            user.UserCredentials = userCredentials;
+
+            _userRepository.Insert(user);
+            _userRepository.Save();
+
+            return userCredentials;
+        }
+
+        public string Login(LoginDTO request)
+        {
+            var user = _userCredentialsRepository.GetByUsername(request.Username);
+            if (user == null)
+                throw new InvalidOperationException("User not found!");
+
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+                throw new UnauthorizedAccessException("Wrong password!");
+
+            string token = CreateToken(user);
+
+            // var refreshToken = GenerateRefreshToken();
+            // SetRefreshToken(refreshToken);
+
+            return token;
+        }
+
+        /*public async Task<string> RefreshToken()
+        {
+            var refreshToken = Request.Cookies["refreshToken"];
+
+            if (!user.RefreshToken.Equals(refreshToken))
+            {
+                return Unauthorized("Invalid Refresh Token.");
+            }
+            else if (user.TokenExpires < DateTime.Now)
+            {
+                return Unauthorized("Token expired.");
+            }
+
+            string token = CreateToken(user);
+            var newRefreshToken = GenerateRefreshToken();
+            SetRefreshToken(newRefreshToken, user);
+
+            return token;
+        }
+
+        private RefreshToken GenerateRefreshToken()
+        {
+            var refreshToken = new RefreshToken
+            {
+                Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+                Expires = DateTime.Now.AddDays(7),
+                Created = DateTime.Now
+            };
+
+            return refreshToken;
+        }
+
+        private void SetRefreshToken(RefreshToken newRefreshToken, UserCredentials user)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Expires = newRefreshToken.Expires
+            };
+            Response.Cookies.Append("refreshToken", newRefreshToken.Token, cookieOptions);
+
+            user.RefreshToken = newRefreshToken.Token;
+            user.TokenCreated = newRefreshToken.Created;
+            user.TokenExpires = newRefreshToken.Expires;
+        }*/
+
+        private string CreateToken(UserCredentials user)
+        {
+            List<Claim> claims = new()
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, user.UserRole.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value ?? string.Empty));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+    }
+}
