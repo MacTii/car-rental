5dd766464901bed31e068d3ec2bdbb141271325e
MacTii <mactii41@gmail.com>
2023-05-09 18:25:52 +0200

    refactor: fix enum names

--- Domain/Enums/GenderEnum.cs
+++ Domain/Enums/GenderEnum.cs
@@ -8,8 +8,8 @@ namespace Domain.Enums
 {
     public enum GenderEnum
     {
-        MALE,
-        FEMALE,
-        OTHER
+        Male,
+        Female,
+        Other
     }
 }
--- Domain/Enums/UserRoleEnum.cs
+++ Domain/Enums/UserRoleEnum.cs
@@ -8,7 +8,7 @@ namespace Domain.Enums
 {
     public enum UserRoleEnum
     {
-        USER,
-        ADMIN
+        User,
+        Admin
     }
 }
