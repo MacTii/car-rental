eafc54f957798930e33781fd414fc1216dc2f77d
MacTii <mactii41@gmail.com>
2023-05-14 00:18:56 +0200

    chore: add http nuget package

--- API/Program.cs
+++ API/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddTransient<GlobalExceptionHandlingMiddleware>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHttpContextAccessor();
 
 // Swagger authorization
 builder.Services.AddSwaggerGen(c =>
