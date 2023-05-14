fcd4a90d8c0bc4060534c00deb973eb336b3e002
MacTii <mactii41@gmail.com>
2023-05-14 15:08:23 +0200

    feature: add request logging middleware

---
+++ API/Middlewares/RequestLoggingMiddleware.cs
@@ -0,0 +1,27 @@
+﻿namespace API.Middlewares
+{
+    public class RequestLoggingMiddleware : IMiddleware
+    {
+        private readonly ILogger<RequestLoggingMiddleware> _logger;
+
+        public RequestLoggingMiddleware(ILogger<RequestLoggingMiddleware> logger)
+            => _logger = logger;
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var username = GetMyUsername(context);
+            _logger.LogInformation($"Request: {context.Request.Method} {context.Request.Path} - User: {username}");
+
+            await next(context);
+        }
+
+        private static string GetMyUsername(HttpContext httpContext)
+        {
+            if (httpContext?.User?.Identity?.Name != null)
+            {
+                return httpContext.User.Identity.Name;
+            }
+            return string.Empty;
+        }
+    }
+}
