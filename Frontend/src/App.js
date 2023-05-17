223fe6b9372bf8f21109d80445b20634474f90bb
MacTii <mactii41@gmail.com>
2023-05-17 21:12:17 +0200

    feature: implement logic in main class

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
 
 function App() {
-  return (
-    <h1>Hello world!</h1>
-  );
+  return <Layout />;
 }
 
 export default App;
