2bafbc87d96b08e53b095d3170ef5e4bc5b54a71
MacTii <mactii41@gmail.com>
2023-05-23 21:59:08 +0200

    feature: modify Home component

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+import HeroSlider from "../components/UI/HeroSlider";
+import Helmet from "../components/Helmet/Helmet";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Helmet title="Home">
+      <section className="p-0 hero__slider-section">
+        <HeroSlider />
+      </section>
+    </Helmet>
+  );
 };
 
 export default Home;
