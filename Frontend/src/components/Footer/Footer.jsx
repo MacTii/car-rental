1bc295097651956b6eb7165ab8147dace2f1071f
MacTii <mactii41@gmail.com>
2023-05-23 21:58:07 +0200

    feature: add Footer implementation

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,105 @@
-import React from 'react'
+import React from "react";
+
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../../styles/footer.css";
+
+const quickLinks = [
+  {
+    path: "/about",
+    display: "About",
+  },
+  {
+    path: "#",
+    display: "Privacy Policy",
+  },
+  {
+    path: "/cars",
+    display: "Car Listing",
+  },
+  {
+    path: "/blogs",
+    display: "Blog",
+  },
+  {
+    path: "/contact",
+    display: "Contact",
+  },
+];
 
 const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <Container>
+        <Row>
+          <Col lg="4" md="4" sm="12">
+            <div className="logo footer__logo">
+              <h1>
+                <Link to="/home" className="d-flex align-items-center gap-3">
+                  <i class="ri-car-line"></i>
+                  <span>
+                    Rent Car <br /> Service
+                  </span>
+                </Link>
+              </h1>
+            </div>
+            <p className="footer__logo-content">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Explicabo numquam vel quam maxime commodi voluptatem iste natus
+              fugit beatae itaque magnam, laudantium et impedit repellendus,
+              quis deserunt? Vitae, est cum?
+            </p>
+          </Col>
 
-export default Footer
+          <Col lg="2" md="4" sm="6">
+            <div className="mb-4">
+              <h5 className="footer__link-title">Quick Links</h5>
+              <ListGroup>
+                {quickLinks.map((item, index) => (
+                  <ListGroupItem key={index} className="p-0 mt-3 quick__link">
+                    <Link to={item.path}>{item.display}</Link>
+                  </ListGroupItem>
+                ))}
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <div className="mb-4">
+              <h5 className="footer__link-title mb-4">Head Office</h5>
+              <p className="office__info">123 Zindabazar, Sylhet, Bangladesh</p>
+              <p className="office__info">Phone: +08882343124</p>
+              <p className="office__info">Email: macti123@gmail.com</p>
+              <p className="office__info">Office Time: 10am - 7pm</p>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4">
+            <div className="mb-4">
+              <h5 className="footer__link-title">Newsletter</h5>
+              <p className="section__description">Subscribe our newsletter</p>
+              <div className="newsletter">
+                <input type="email" placeholder="Email" />
+                <span>
+                  <i class="ri-send-plane-line"></i>
+                </span>
+              </div>
+            </div>
+          </Col>
+          <Col lg="12">
+            <div className="footer__bottom">
+              <p className="section__description d-flex align-items-center justify-content-center gap-1 pt-4">
+                <i class="ri-copyright-line"></i> Copyright {year}, Developed by
+                Mateusz Kapka. All rights reserved.
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Footer;
