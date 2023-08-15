71ee7964dcb8057895c50eee03501487f8cbda2b
MacTii <mactii41@gmail.com>
2023-08-15 21:36:43 +0200

    feat: add migration

---
+++ Infrastructure/Persistence/Migrations/20230815140257_car-model-engine.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class carmodelengine : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Automatic",
+                table: "Cars",
+                newName: "Engine");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Engine",
+                table: "Cars",
+                newName: "Automatic");
+        }
+    }
+}
