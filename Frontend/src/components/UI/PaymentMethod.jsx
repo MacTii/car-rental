de456439431de2cb707f621bed63329d2c560e3b
MacTii <mactii41@gmail.com>
2023-08-17 16:46:06 +0200

    feat: implement payment method with radio buttons

--- Frontend/src/components/UI/PaymentMethod.jsx
+++ Frontend/src/components/UI/PaymentMethod.jsx
@@ -1,27 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 import masterCard from "../../assets/all-images/master-card.jpg";
 import paypal from "../../assets/all-images/paypal.jpg";
-import '../../styles/payment-method.css';
+import "../../styles/payment-method.css";
 
 const PaymentMethod = () => {
+  const [selectedPayment, setSelectedPayment] = useState("directBankTransfer");
+
+  const handlePaymentChange = (event) => {
+    setSelectedPayment(event.target.value);
+  };
+
   return (
     <>
       <div className="payment">
         <label htmlFor="" className="d-flex align-items-center gap-2">
-          <input type="radio" /> Direct Bank Transfer
+          <input
+            type="radio"
+            name="paymentMethod"
+            value="directBankTransfer"
+            checked={selectedPayment === "directBankTransfer"}
+            onChange={handlePaymentChange}
+          />
+          Direct Bank Transfer
         </label>
       </div>
 
       <div className="payment mt-3">
         <label htmlFor="" className="d-flex align-items-center gap-2">
-          <input type="radio" /> Cheque Payment
+          <input
+            type="radio"
+            name="paymentMethod"
+            value="chequePayment"
+            checked={selectedPayment === "chequePayment"}
+            onChange={handlePaymentChange}
+          />
+          Cheque Payment
         </label>
       </div>
 
       <div className="payment mt-3 d-flex align-items-center justify-content-between">
         <label htmlFor="" className="d-flex align-items-center gap-2">
-          <input type="radio" /> Master Card
+          <input
+            type="radio"
+            name="paymentMethod"
+            value="masterCard"
+            checked={selectedPayment === "masterCard"}
+            onChange={handlePaymentChange}
+          />
+          Master Card
         </label>
 
         <img src={masterCard} alt="" />
@@ -29,13 +56,17 @@ const PaymentMethod = () => {
 
       <div className="payment mt-3 d-flex align-items-center justify-content-between">
         <label htmlFor="" className="d-flex align-items-center gap-2">
-          <input type="radio" /> Paypal
+          <input
+            type="radio"
+            name="paymentMethod"
+            value="paypal"
+            checked={selectedPayment === "paypal"}
+            onChange={handlePaymentChange}
+          />
+          Paypal
         </label>
 
         <img src={paypal} alt="" />
-      </div>
-      <div className="payment text-end mt-5">
-        <button>Reserve Now</button>
       </div>
     </>
   );
