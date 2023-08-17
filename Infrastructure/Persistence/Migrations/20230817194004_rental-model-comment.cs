83d3612925dd29b2756c338981c2c42adf4c2ab0
MacTii <mactii41@gmail.com>
2023-08-17 23:45:49 +0200

    feature: add rental migration

---
+++ Infrastructure/Persistence/Migrations/20230817194004_rental-model-comment.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class rentalmodelcomment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Comments",
+                table: "Rentals",
+                newName: "Comment");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Comment",
+                table: "Rentals",
+                newName: "Comments");
+        }
+    }
+}
