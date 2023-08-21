c8148d3661b24b1ecec12155ce7d0d2000da2118
MacTii <mactii41@gmail.com>
2023-08-21 21:59:22 +0200

    feat: update component and pages

--- Frontend/src/components/UI/FindCarForm.jsx
+++ Frontend/src/components/UI/FindCarForm.jsx
@@ -1,36 +1,37 @@
 import React from "react";
 import "../../styles/find-car-form.css";
 import { Form, FormGroup } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const FindCarForm = () => {
   return (
     <Form className="form">
       <div className="d-flex align-items-center justify-content-between flex-wrap">
         <FormGroup className="form__group">
-          <input type="text" placeholder="From address" required />
+          <input type="text" placeholder="Make" required />
         </FormGroup>
 
         <FormGroup className="form__group">
-          <input type="text" placeholder="To address" required />
+          <input type="text" placeholder="Model" required />
         </FormGroup>
 
         <FormGroup className="form__group">
-          <input type="date" placeholder="Journey date" required />
+          <input type="date" placeholder="Year" required />
         </FormGroup>
 
         <FormGroup className="form__group">
-          <input className="journey__time" type="time" placeholder="Journey time" required />
+          <input type="text" placeholder="Price Per Day" required />
         </FormGroup>
 
         <FormGroup className="select__group">
           <select>
-            <option value="ac">AC Car</option>
-            <option value="non-ac">Non AC Car</option>
+            <option value="White">White</option>
+            <option value="Black">Black</option>
           </select>
         </FormGroup>
 
         <FormGroup className="form__group">
-          <button className="btn find__car-btn">Find Car</button>
+          <Link to='/cars' className="btn find__car-btn">Find Car</Link>
         </FormGroup>
       </div>
     </Form>
