2a34cee90d2c7cf273cb3911b6baa551aca4d565
MacTii <mactii41@gmail.com>
2023-08-27 19:48:37 +0200

    feat: add comments for mapping

--- Application/Mapper/MappingProfile.cs
+++ Application/Mapper/MappingProfile.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application.Helpers;
+using Domain.Enums;
 
 namespace Application.Mapper
 {
@@ -13,33 +15,54 @@ namespace Application.Mapper
     {
         public MappingProfile()
         {
+            // UserDTO -> User
             CreateMap<UserDTO, User>()
                 .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => DateTime.ParseExact(src.DateOfBirth, "yyyy-MM-dd", null)));
+
+            // User -> UserDTO
             CreateMap<User, UserDTO>()
                 .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth.ToString("yyyy-MM-dd")));
 
+            // CarDTO -> Car
             CreateMap<CarDTO, Car>()
-                .ForMember(dest => dest.Image, opt => opt.MapFrom(src => Convert.FromBase64String(src.Image)));
-            CreateMap<Car, CarDTO>()
-                .ForMember(dest => dest.Image, opt => opt.MapFrom(src => Convert.ToBase64String(src.Image)));
+                .ForMember(dest => dest.Image, opt => opt.MapFrom(src => Base64ImageConverter.RemoveBase64Prefix(src.Image)));
 
+            // Car -> CarDTO
+            CreateMap<Car, CarDTO>()
+                .ForMember(dest => dest.Image, opt => opt.MapFrom(src => Base64ImageConverter.AddBase64Prefix(src.Image)));
+
+            // RentalDTO -> Rental
             CreateMap<RentalDTO, Rental>();
+
+            // Rental -> RentalDTO
             CreateMap<Rental, RentalDTO>();
 
+            // RegisterDTO -> UserCredentials
             CreateMap<RegisterDTO, UserCredentials>();
+
+            // RegisterDTO -> User
             CreateMap<RegisterDTO, User>()
                 .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => DateTime.ParseExact(src.DateOfBirth, "yyyy-MM-dd", null)));
 
+            // UserCredentialsDTO -> UserCredentials
             CreateMap<UserCredentialsDTO, UserCredentials>();
+
+            // UserCredentials -> UserCredentialsDTO
             CreateMap<UserCredentials, UserCredentialsDTO>();
 
-            CreateMap<Blog, BlogDTO>()
-                .ForMember(dest => dest.Image, opt => opt.MapFrom(src => Convert.ToBase64String(src.Image)));
+            // BlogDTO -> Blog
             CreateMap<BlogDTO, Blog>()
-                .ForMember(dest => dest.Image, opt => opt.MapFrom(src => Convert.FromBase64String(src.Image)));
+                .ForMember(dest => dest.Image, opt => opt.MapFrom(src => Base64ImageConverter.RemoveBase64Prefix(src.Image)));
 
-            CreateMap<Comment, CommentDTO>();
+            // Blog -> BlogDTO
+            CreateMap<Blog, BlogDTO>()
+                .ForMember(dest => dest.Image, opt => opt.MapFrom(src => Base64ImageConverter.AddBase64Prefix(src.Image)));
+
+            // CommentDTO -> Comment
             CreateMap<CommentDTO, Comment>();
+
+            // Comment -> CommentDTO
+            CreateMap<Comment, CommentDTO>();
         }
     }
 }
