f769f4d79855e5b8fff5e26e2cc4c134e0acf55c
MacTii <mactii41@gmail.com>
2023-09-03 19:24:18 +0200

    feat: refactor controllers

---
+++ Presentation/Controllers/RegistrationController.cs
@@ -0,0 +1,43 @@
+﻿using Application.Interfaces.Services;
+using Application.Mapper.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    public class RegistrationController : ControllerBase
+    {
+        #region Injection
+
+        private readonly IConfiguration _configuration;
+        private readonly IRegistrationService _registrationService;
+
+        public RegistrationController(IConfiguration configuration, IRegistrationService registrationService)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _registrationService = registrationService ?? throw new ArgumentNullException(nameof(registrationService));
+        }
+
+        #endregion Injection
+
+        [HttpPost("register")]
+        public ActionResult Register(RegisterDTO request)
+        {
+            if (request == null)
+                return BadRequest("Invalid input data"); // return 400 Bad Request
+
+            var token = _registrationService.Register(request);
+            return Ok(
+                new
+                {
+                    Response = "User registered successfully",
+                    Data = token
+                });
+        }
+    }
+}
